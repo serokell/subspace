61047014c66d6a5fd488b110ca2a83e93d7b0944
notgne2 <gen2@gen2.space>
2022-02-26 23:39:14 -0700

    Fix NixOS module

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
           goPackagePath = "github.com/subspacecommunity/subspace";
           version = "1.5.0";
         in
-        # deps
         pkgs.buildGoPackage {
           inherit goPackagePath version;
           src = ./.;
@@ -46,46 +45,112 @@
         }
       );
 
-      # nixosConfigurations = {
-      #   # testContainer =  {}
-      # };
-
       nixosModule = { pkgs, lib, config, ... }:
         with lib;
         let
-          subspace = self.defaultPackage."${config.system}";
           cfg = config.services.subspace;
         in
         {
-          options = {
-            services.subspace = {
-              enable = mkEnableOption "subspace";
-              dataDir = mkOption {
-                description = "Path to data folder";
-                default = "/var/subspace/data";
-                type = types.path;
-              };
-              privateKeyFile = {
-                description = "Path to Wireguard private key";
-                default = "/secrets/subspace.private";
-                type = types.path;
-              };
-              params = mkOption {
-                description = "Parameters for Subspace binary";
-                default = "--http-host localhost -http-addr \":3331\" -http-insecure";
-                type = types.str;
-              };
+          options.services.subspace = {
+            enable = mkEnableOption "subspace";
+
+            package = mkOption {
+              description = "A package from which to take subspace";
+              default = self.defaultPackage.${pkgs.system};
+              type = types.package;
             };
 
+            privateKeyFile = mkOption {
+              description = "Path to Wireguard private key";
+              default = "/secrets/subspace.private";
+              type = types.str;
+            };
+
+            user = mkOption {
+              description = "User account under which Subspace runs.";
+              default = "subspace";
+              type = types.str;
+            };
+            group = mkOption {
+              description = "Group account under which Subspace runs.";
+              default = "subspace";
+              type = types.str;
+            };
+
+            httpHost = mkOption {
+              description = "The host to listen on and set cookies for";
+              default = "localhost";
+              type = types.str;
+            };
+            backlink = mkOption {
+              description = "The page to set the home button to";
+              default = "/";
+              type = types.str;
+            };
+            dataDir = mkOption {
+              description = "Path to data folder";
+              default = "/var/lib/subspace";
+              type = types.str;
+            };
+            debug = mkOption {
+              description = "Place subspace into debug mode for verbose log output";
+              default = false;
+              type = types.bool;
+            };
+            httpInsecure = mkOption {
+              description = "enable session cookies for http and remove redirect to https";
+              default = false;
+              type = types.bool;
+            };
+            letsencrypt = mkOption {
+              description = "Whether or not to use a LetsEncrypt certificate";
+              default = true;
+              type = types.bool;
+            };
+            httpAddr = mkOption {
+              description = "HTTP Listen address";
+              default = ":3331";
+              type = types.str;
+            };
+
+            params = mkOption {
+              description = "Parameters for Subspace binary";
+              default = "";
+              type = types.str;
+            };
           };
+
           config = mkIf cfg.enable {
+            users.users = optionalAttrs (cfg.user == "subspace") ({
+              subspace = {
+                isSystemUser = true;
+                group = cfg.group;
+                # uid = config.ids.uids.subspace;
+                description = "Subspace WireGuard GUI user";
+                home = cfg.dataDir;
+              };
+            });
+
+            users.groups = optionalAttrs (cfg.group == "subspace") ({
+              subspace = {
+                # gid = config.ids.gids.subspace;
+              };
+            });
+
+            systemd.tmpfiles.rules = [ "d ${cfg.dataDir} 0750 ${cfg.user} ${cfg.group}" ];
+
             systemd.services.subspace = {
-              description = "AMule daemon";
+              description = "A simple WireGuard VPN server GUI";
               wantedBy = [ "multi-user.target" ];
               after = [ "network.target" ];
 
+              serviceConfig = {
+                User = cfg.user;
+                Group = cfg.group;
+                WorkingDirectory = "${cfg.package}/libexec";
+              };
+
               preStart = ''
-                mkdir -p ${cfg.dataDir}
                 pushd ${cfg.dataDir}
 
                 mkdir -p wireguard/clients
@@ -96,21 +161,23 @@
 
                 cp ${cfg.privateKeyFile} wireguard/server.private
                 cat ${cfg.privateKeyFile} | ${pkgs.wireguard-tools}/bin/wg pubkey > server.public
-
-                chmod -R u+r a-rwx ${user} ${cfg.dataDir}
-                chown -r ${user} ${cfg.dataDir}
               '';
 
               script = ''
-                cd ${subspace}/libexec
-                ${subspace}/bin/subspace \
-                  -datadir=${cfg.dataDir} \
+                ${cfg.package}/bin/subspace \
+                  --http-host="${cfg.httpHost}" \
+                  --backlink="${cfg.backlink}" \
+                  --datadir="${cfg.dataDir}" \
+                  --debug="${if cfg.debug then "true" else "false"}" \
+                  --http-addr="${cfg.httpAddr}" \
+                  --http-insecure="${if cfg.httpInsecure then "true" else "false"}" \
+                  --letsencrypt="${if cfg.letsencrypt then "true" else "false"}" \
                   ${cfg.params}
               '';
             };
           };
-        };
-
+        }
+      ;
     };
 
 }
